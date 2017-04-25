3781e3458387c8767bc01170f795be2faff3e35e
Noah Panicola <noahpanicola@gmail.com>
2017-04-25 11:29:28 -0500

    testing

---
+++ CommandLineBaseball/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CommandLineBaseball
+//
+//  Created by Noah Panicola on 4/25/17.
+//  Copyright © 2017 self.edu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
