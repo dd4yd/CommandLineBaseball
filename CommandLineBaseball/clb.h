ae67e7a5615e9f68fcc65fc557c95ed999320871
Noah Panicola <noahpanicola@gmail.com>
2017-04-25 12:17:22 -0500

    changed header

--- CommandLineBaseball/clb.h
+++ CommandLineBaseball/clb.h
@@ -1,6 +1,28 @@
 //libraries
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
 
 //structs
+typedef struct player {
+    char name[100];
+    int power;
+    int contact;
+    int pitching;
+    struct player *next;
+} Player;
+
+typdef struct game {
+    Player *team1;
+    Player *team2;
+    int scoreboard[18];
+    int outs;
+    int strikes;
+    int balls;
+    int bases[4];
+    
+} Game;
 
 //prototypes go here
