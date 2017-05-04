e2742f29c0e48e3cc8dd70dfc377eac5c45a05c0
dd4yd <dd4yd@mail.missouri.edu>
2017-05-03 21:37:50 -0500

    sort finished

--- CommandLineBaseball/david.c
+++ CommandLineBaseball/david.c
@@ -16,31 +16,7 @@ int maxIndex(Player list[], int n, int i, int j, int k, int field){
     //field: 1->contact, 2->power, 3->pitching, 4->name
     int max = i;
     switch(field){
-        case 1:
-            if(j < n && list[j].contact > list[max].contact){
-                max = j;
-            }
-            if(k < n && list[k].contact > list[max].contact){
-                max = k;
-            }
-            break;
-        case 2:
-            if(j < n && list[j].power > list[max].power){
-                max = j;
-            }
-            if(k < n && list[k].power > list[max].power){
-                max = k;
-            }
-            break;
-        case 3:
-            if(j < n && list[j].pitching > list[max].pitching){
-                max = j;
-            }
-            if(k < n && list[k].pitching > list[max].pitching){
-                max = k;
-            }
-            break;
-        case 4: //sort by last name only
+        case 1: //last name
             if(j < n && strcmp(list[j].last, list[max].last) > 0){
                 max = j;
             }
@@ -48,6 +24,31 @@ int maxIndex(Player list[], int n, int i, int j, int k, int field){
                 max = k;
             }
             break;
+        case 2: //contact
+            if(j < n && list[j].contact > list[max].contact){
+                max = j;
+            }
+            if(k < n && list[k].contact > list[max].contact){
+                max = k;
+            }
+            break;
+        case 3: //power
+            if(j < n && list[j].power > list[max].power){
+                max = j;
+            }
+            if(k < n && list[k].power > list[max].power){
+                max = k;
+            }
+            break;
+        case 4: //pitching
+            if(j < n && list[j].pitching > list[max].pitching){
+                max = j;
+            }
+            if(k < n && list[k].pitching > list[max].pitching){
+                max = k;
+            }
+            break;
+
         default:
             max = i;
             break;
@@ -60,16 +61,25 @@ void downheap(Player list[], int n, int i, int field){
         int j = maxIndex(list, n, i, 2*i+1, 2*i+2, field);
         if(j == i) break;
         
-        Player person = list[i];
+        Player player = list[i];
         list[i] = list[j];
-        list[j] = person;
+        list[j] = player;
         i = j;
     }
 }
 
 Player* sortPlayerList(Player list[], int n, int field){
     
-    //field: 1->contact, 2->power, 3->pitching, 4->name
-    
+    //field: 1->name, 2->contact, 3->power, 4->pitching
+    int i;
+    for(i = (n-2)/2; i >= 0; i--){
+        downheap(list, n, i, field);
+    }
+    for(i = 0; i < n; i++){
+        Player player = list[n-1-i];
+        list[n-i-1] = list[0];
+        list[0] = player;
+    }
+    return list;
 }
 
