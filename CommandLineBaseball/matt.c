6d61d478be5c75de41d9ada7b9da7748f912b319
Matthew Barber <mgbz89@mail.missouri.edu>
2017-05-04 15:19:33 -0500

    Add summary of inning

--- CommandLineBaseball/matt.c
+++ CommandLineBaseball/matt.c
@@ -323,21 +323,25 @@ void simInning(Game game){
     
     if(num >= 3){
         game.scoreboard[game.inning - 1] = 0;
+        printf("%s pitched in inning %d.\nThe computer team had %d hits and 0 runs\n", game.user_pitcher->last, game.inning/2, rand() % 3);
         return;
     }
     if(num >= 2){
         game.scoreboard[game.inning - 1] = 1;
         game.cpu_score += 1;
+        printf("%s pitched in inning %d.\nThe computer team had %d hits and 1 run\n", game.user_pitcher->last, game.inning/2, rand() % 3 + 1);
         return;
     }
     if(num >= 1){
         game.scoreboard[game.inning - 1] = 2;
         game.cpu_score += 2;
+        printf("%s pitched in inning %d.\nThe computer team had %d hits and 2 runs\n", game.user_pitcher->last, game.inning/2, rand() % 4 + 1);
         return;
     }
     if(num >= 0){
         game.scoreboard[game.inning - 1] = 3;
         game.cpu_score += 3;
+        printf("%s pitched in inning %d.\nThe computer team had %d hits and 3 runs\n", game.user_pitcher->last, game.inning/2, rand() % 5 + 2);
         return;
     }
 }
